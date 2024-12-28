acba1b81a00511caa34fe987653bc79792e0e860
tictaktoe333 <tom.ski@hotmail.co.uk>
2024-12-28 18:35:42 +0000

    server basics

--- messenger/src/server.py
+++ messenger/src/server.py
@@ -1,7 +1,8 @@
 import logging
 import selectors
 import socket
-import threading
+import yaml
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -10,12 +11,21 @@ sel = selectors.DefaultSelector()
 
 
 class Server:
-    def __init__(self, host="localhost", port=8080):
-        self.host = host
-        self.port = port
+    def __init__(self):
+        self.host = None
+        self.port = None
+        self.concurrent_connections = None
+        with open("config.yaml", "r") as f:
+            config = yaml.safe_load(f)
+            logger.debug("Config loaded:", config)
+        self.host = config.get("server", {}).get("host", "localhost")
+        self.port = config.get("server", {}).get("port", 8080)
+        self.concurrent_connections = config.get("server", {}).get(
+            "concurrent_connections", 10
+        )
         self.clients = set()
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((host, port))
-        self.server_socket.listen()
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(self.concurrent_connections)
         logger.info(f"Server listening on {self.host}:{self.port}")
         sel.register(self.server_socket, selectors.EVENT_READ)
